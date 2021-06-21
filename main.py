658e5573598c85039b858638a852ae73f93018dc
Evison <evisoncy@gmail.com>
2021-06-21 18:45:22 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from aplicacion import *
+
+
+
+# Recuerda tener todos los archivos en una misma carpeta
+# para que los imports no tengan problemas.
+if __name__ == '__main__':
+    aplicacion = aplicacion ()
+    aplicacion.on_execute()
